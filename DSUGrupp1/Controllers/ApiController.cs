657de2402f3a6e10340132ccd6001bb613100ce8
Linus Lithner <lili1010@student.miun.se>
2024-01-22 15:18:26 +0100

    Added models and Controller for Api calls to SCB

---
+++ DSUGrupp1/Controllers/ApiController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using DSUGrupp1.Models.API;
+using System.Text;
+using System.Net.Http;
+
+
+namespace DSUGrupp1.Controllers
+{
+    [Route("[controller]")]
+    public class ApiController : Controller
+    {
+        private readonly HttpClient? _httpClient;
+
+        public ApiController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MakeApiCall()
+        {
+            var apiQuery = new ApiQuery
+            {
+                Query = new List<QueryItem>
+                {
+                    new QueryItem
+                    {
+                        Code = "Region",
+                        Selection = new Selection { Filter = "vs:RegionKommun07", Values = new List<string> { "2380" } }
+                    },
+                    new QueryItem
+                    {
+                        Code = "ContentsCode",
+                        Selection = new Selection { Filter = "item", Values = new List<string> { "BE0101N1" } }
+                    },
+                     new QueryItem
+                    {
+                        Code = "Tid",
+                        Selection = new Selection { Filter = "item", Values = new List<string> { "2022" } }
+                    },
+
+                },
+                Response = new Response { Format = "json" }
+            };
+
+            string jsonRequest = JsonConvert.SerializeObject(apiQuery);
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _httpClient.PostAsync("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningNy", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    return Ok(responseContent);
+                }
+
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+               
+                return StatusCode(500, ex.Message);
+            }
+        }
+        }
+}
---
+++ DSUGrupp1/Models/API/ApiQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace DSUGrupp1.Models.API
+{
+    public class ApiQuery
+    {
+        public List<QueryItem>? Query { get; set; }
+        public Response? Response { get; set; }
+    }
+}
