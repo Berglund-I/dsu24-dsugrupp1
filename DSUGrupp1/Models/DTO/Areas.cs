ed504d624f133f37dd296e9e3ff37b23d438e3b0
Linus Lithner <lili1010@student.miun.se>
2024-01-25 14:18:26 +0100

    Added dto models GetDesoInfo for Api call

---
+++ DSUGrupp1/Models/DTO/Areas.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace DSUGrupp1.Models.DTO
+{
+	public class Areas
+	{
+		[JsonProperty("deso")]
+		string? Deso { get; set; }
+		[JsonProperty("deso-name")]
+		string? DesoName { get; set; }
+	}
+}
