77976d3c8945539e74d31741bf0a1f76a7fe3131
jonnaastiernstrom <jonnaastiernstrom@hotmail.com>
2024-01-29 22:15:50 +0100

    Skapat loop för alla deson
    gjort metod för att räkna hur många män och kvinnor som är vaccinerade
    
    #46 #47

--- DSUGrupp1/Models/DTO/VaccinationDataFromSpecificDeSoDto.cs
+++ DSUGrupp1/Models/DTO/VaccinationDataFromSpecificDeSoDto.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System.Collections;
 
 namespace DSUGrupp1.Models.DTO
 {
     public class VaccinationDataFromSpecificDeSoDto
     {
         public TotalVaccinationsDto Meta { get; set; }
+
+        [JsonProperty("patients")]
+        public List<PatientInformationDto> Patients { get; set; }
 
     }
 }
---
+++ DSUGrupp1/Models/ViewModels/DisplayGenderStatisticsViewModel.cs
@@ -0,0 +1,51 @@
+﻿using DSUGrupp1.Models.DTO;
+
+namespace DSUGrupp1.Models.ViewModels
+{
+    public class DisplayGenderStatisticsViewModel
+    {
+        public int PopulationFemales { get; set; }
+        public int VaccinatedFemales { get; set; }
+        public int PopulationMales { get; set; }
+        public int VaccinatiedMales { get; set; }
+
+
+        List<VaccinationDataFromSpecificDeSoDto> _vaccinationDataFromSpecificDeSoDto = null;
+
+        public DisplayGenderStatisticsViewModel(PopulationDto population, List<VaccinationDataFromSpecificDeSoDto> vaccinationDataFromSpecificDeSoDto)
+        {
+            PopulationMales = int.Parse(population.Data[0].Values[0]);
+            PopulationFemales = int.Parse(population.Data[1].Values[0]);
+            _vaccinationDataFromSpecificDeSoDto = vaccinationDataFromSpecificDeSoDto;
+            CountVaccinatedGender();
+
+
+        }
+
+        /// <summary>
+        /// This method counts the number of vaccinated males and females by iterating through vaccination data for specific Deso codes. 
+        /// It checks the gender of each patient and increments counters accordingly, returning an array with the count of vaccinated males and females.
+        /// </summary>
+        /// <returns></returns>
+        public void CountVaccinatedGender()
+        {
+            foreach (var list in _vaccinationDataFromSpecificDeSoDto)
+            {
+                foreach (var patient in list.Patients)
+                {
+                    if (patient.Gender == "Male")
+                    {
+                        VaccinatiedMales++;
+                    }
+                    else if (patient.Gender == "Female")
+                    {
+                        VaccinatedFemales++;
+                    }
+                }
+            }
+
+
+        }
+
+    }
+}
